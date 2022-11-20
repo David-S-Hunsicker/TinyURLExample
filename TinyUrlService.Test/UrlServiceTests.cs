52fd66710c35d163dd7830f8db9e6751b197dd95
David Hunsicker <David.S.Hunsicker@gmail.com>
2022-11-19 16:00:24 -0800

    adding UT

--- TinyUrlService.Test/UrlServiceTests.cs
+++ TinyUrlService.Test/UrlServiceTests.cs
@@ -1,11 +1,39 @@
+using TinyUrlService.BusinessLogic;
+using TinyUrlService.Persistence;
+
 namespace TinyUrlService.Test
 {
     [TestClass]
     public class UrlServiceTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldResolveAddedEntry()
         {
+            // Assign
+            var longUrl = "example.com";
+            var r = new UrlResolver();
+            var a = new UrlAdder();
+            // Act
+
+            var shortUrl = a.CreateRecord(longUrl);
+            // Assert
+            Assert.AreEqual(longUrl, r.Resolve(shortUrl));
+        }
+
+        [TestMethod]
+        public void ShouldResolveDuplicateEntries()
+        {
+            // Assign
+            var longUrl = "example.com";
+            var r = new UrlResolver();
+            var a = new UrlAdder();
+            // Act
+
+            var shortUrl = a.CreateRecord(longUrl);
+            var shortUrl2 = a.CreateRecord(longUrl);
+            // Assert
+            Assert.AreEqual(r.Resolve(shortUrl2), r.Resolve(shortUrl));
+
         }
     }
 }
